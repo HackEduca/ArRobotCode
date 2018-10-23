4ad1dbe09a43c249aef8b73aba3f658293134020
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-10-23 12:49:02 +0300

    WebKit, JS Alerts are sent to IOS device

--- ArRobotCode/InstructionsViewController.swift
+++ ArRobotCode/InstructionsViewController.swift
@@ -15,19 +15,28 @@ class InstructionsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        instructionsWebView.uiDelegate = self
+        instructionsWebView.navigationDelegate = self
+    
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        // Set up to listen to messages
+        let contentController = WKUserContentController()
+        contentController.add(self, name: "callback")
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        
+        // Load the index.html
         let htmlPath = Bundle.main.path(forResource: "index", ofType: "html")
         let htmlUrl = URL(fileURLWithPath: htmlPath!, isDirectory: false)
         instructionsWebView.loadFileURL(htmlUrl, allowingReadAccessTo: htmlUrl)
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -37,5 +46,27 @@ class InstructionsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension InstructionsViewController: WKScriptMessageHandler, WKNavigationDelegate, WKUIDelegate{
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        //This function handles the events coming from javascript. We'll configure the javascript side of this later.
+        //We can access properties through the message body, like this:
+        guard let response = message.body as? String else {
+            print("Something is fishy")
+            return
+        }
+        print(response)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        //This function is called when the webview finishes navigating to the webpage.
+        //We use this to send data to the webview when it's loaded.
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        print("Alert from WebKIT: " + message)
+        completionHandler()
+    }
+    
 }
