5cfecf67bb122c15f15461c2e1ee66e0d4df697b
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-11-22 17:13:39 +0200

    Working on player position check, implemented a ARGame engine, accessing realm from other thread not working

---
+++ IOS/ArRobotCode/Engine/EngineAR.swift
@@ -0,0 +1,9 @@
+//
+//  EngineAR.swift
+//  ArRobotCode
+//
+//  Created by Sorin Sebastian Mircea on 22/11/2018.
+//  Copyright © 2018 Sorin Sebastian Mircea. All rights reserved.
+//
+
+import Foundation
---
+++ IOS/ArRobotCode/Engine/EngineInterface.swift
@@ -0,0 +1,9 @@
+//
+//  EngineInterface.swift
+//  ArRobotCode
+//
+//  Created by Sorin Sebastian Mircea on 22/11/2018.
+//  Copyright © 2018 Sorin Sebastian Mircea. All rights reserved.
+//
+
+import Foundation
